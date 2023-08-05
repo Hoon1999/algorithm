3f1a4590dc08bad9b6387cf4a9df3b3e4c014c04
Hoon1999 <100833901+Hoon1999@users.noreply.github.com>
2023-08-05 17:47:32 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(false);
+    string str;
+    cin >> str;
+    int i;
+    cin >> i;
+    
+    cout << str[i-1];
+    return 0;
+}
