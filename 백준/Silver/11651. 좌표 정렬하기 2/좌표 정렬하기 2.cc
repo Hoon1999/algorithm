3dceb1fc7c46deacad180581e9c1881055356740
Hoon1999 <100833901+Hoon1999@users.noreply.github.com>
2023-09-08 14:37:55 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 48 ms, Memory: 2680 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+typedef struct {
+  int x;
+  int y;
+} pt;
+
+bool cmp(const pt& a, const pt& b);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    // 변수 선언 및 초기화
+    int n;
+    cin >> n;
+    pt arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i].x >> arr[i].y;
+    }
+    
+    // 구현
+    sort(arr, arr+n, cmp);
+    
+    // 정답 출력
+    for(int i = 0 ; i < n ; i++ ) {
+        cout << arr[i].x << " " << arr[i].y << "\n";
+    }
+    
+    return 0;
+}
+
+bool cmp(const pt& a, const pt& b) {
+    if(a.y < b.y) {
+        return true;
+    }
+    else if(a.y == b.y) {
+        if(a.x < b.x) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+}
