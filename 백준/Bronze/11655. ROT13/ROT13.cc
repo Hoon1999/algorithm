dd5b24d05aa7bd37c7fb88c4d4717aa51d1197df
JeongHoon <100833901+Hoon1999@users.noreply.github.com>
2025-07-10 17:02:53 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -3,22 +3,19 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    string str;
 
-    string input, ret = "";
-    getline(cin, input);
-    for(int i = 0; i < input.length(); ++i) {
-        if(isalpha(input[i])) {
-            if('A' <= input[i] && input[i] <= 'Z')
-                ret += (char)((input[i] -'A' + 13) % 26 + 'A');
-            else
-                ret += (char)((input[i] -'a' + 13) % 26 + 'a');
+    getline(cin, str);
+
+    for(int i = 0; i < str.length(); i++) {
+        if('a' <= str[i] && str[i] <= 'z') {
+            cout << (char)(( str[i] - 'a' + 13)  % 26 + 'a');
         }
-        else {
-            ret += input[i];
+        else if('A' <= str[i] && str[i] <= 'Z') {
+            cout << (char) (( str[i] - 'A' + 13) % 26 + 'A');
         }
+        else
+            cout << str[i];
     }
-    cout << ret;
     return 0;
 }
