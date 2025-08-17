fba8d9af8ffccb15210cd0a336ff442402ac0786
JeongHoon <100833901+Hoon1999@users.noreply.github.com>
2025-08-18 00:33:43 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,38 +1,26 @@
 #include <bits/stdc++.h>
 
-using namespace std; 
+using namespace std;
 
-int fee[3];
-int enter[3];
-int leave[3];
-int cnt, start, last, sum;
+int cost[4];
+int arr[104];
+int enter, leave, sum;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    for(int i = 0; i < 3; ++i) {
-        cin >> fee[i];
-    }
-    
-    for(int i = 0; i < 6; ++i) {
-        if(i%2)
-            cin >> leave[i/2];
-        else
-            cin >> enter[i/2];
-    }
-    
-    start = *min_element(enter, enter+3);
-    last = *max_element(leave, leave+3);
-    sum = 0;
-    for(int i = start; i < last; ++i) {
-        cnt = 0;
-        for(int j = 0; j < 3; ++j) {
-            if(enter[j] <= i && i < leave[j])
-                cnt++;
+    cin >> cost[1] >> cost[2] >> cost[3];
+
+    for(int i = 0; i < 3; i++) {
+        cin >> enter >> leave;
+
+        for(int j = enter; j < leave; j++) {
+            arr[j]++;
         }
-        sum += cnt * fee[cnt-1];
     }
-    cout << sum;
+
+    for(int i = 0; i < 104; i++) {
+        sum += cost[arr[i]] * arr[i];
+    }
+    cout << sum << "\n";
+
     return 0;
 }
