84e6e1ad69d65e9bddd8bb2bc726758fb3019149
JeongHoon <100833901+Hoon1999@users.noreply.github.com>
2025-08-18 00:47:02 +0900

    [Silver IV] Title: 주몽, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -2,28 +2,19 @@
 
 using namespace std;
 
-int n, m, cnt, sum;
-int nums[15004];
-vector<int> v;
+int n, m, cnt = 0;
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    // Logic : n Combination 2 으로 나온 값의 합이 m과 동일한 경우 카운트
     cin >> n >> m;
-    
-    // 숫자들을 전부 보관한다.
-    for(int i = 0; i < n; ++i) {
-        cin >> nums[i];
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-    
-    // n Combination 2
-    for(int i = 0; i < n; ++i) {
-        for(int j = i+1; j < n; ++j) {
-            sum = nums[i] + nums[j];
-            if(sum == m)
+    for(int i = 0; i < n-1; i++) {
+        for(int j = i+1; j < n; j++) {
+            if((arr[i] + arr[j]) == m)
                 cnt++;
         }
     }
-    cout << cnt;
+    cout << cnt << "\n";
     return 0;
 }
