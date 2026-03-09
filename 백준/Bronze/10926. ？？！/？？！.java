0bda376f67f98d8d9477e602a627e07de3deeda9
JeongHoon <100833901+Hoon1999@users.noreply.github.com>
2026-03-09 20:48:34 +0900

    [Bronze V] Title: ??!, Time: 164 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String str;
+        Scanner sc = new Scanner(System.in);
+        
+        // 입력받음
+        str = sc.nextLine();
+        
+        // ??! 붙여서 출력
+        System.out.println(str + "??!");
+    }
+}
